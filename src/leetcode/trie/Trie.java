6edba1238542a6db9e1ada7e66803ab10a5f62a9
lllxin037 <lllxin@hotmail.com>
2015-05-17 21:46:58 -0700

    Update Trie.java

--- src/leetcode/trie/Trie.java
+++ src/leetcode/trie/Trie.java
@@ -2,14 +2,11 @@ class TrieNode {
     
     private char c;
     private boolean isLeaf;
+    private TrieNode[26] children = new TrieNode[26];
     
     public TrieNode() {
     }
-    
-    public TrieNode(char c) {
-        this(c, false);
-    }
-    
+
     public TrieNode(char c, boolean leaf) {
         this.c = c;
         this.isLeaf = leaf;
@@ -26,18 +23,59 @@ public class Trie {
 
     // Inserts a word into the trie.
     public void insert(String word) {
-        
+        if (word == null || word.isEmpty())
+            return;
+            
+        TrieNode cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            
+            char c = word.charAt(i);
+            if (cur.children[ c - 'a'] == null)
+                cur.children[ c-'a'] = new TrieNode(c, i == word.length() -1 );
+            
+            cur = cur.children[c-'a'];
+        }
     }
 
     // Returns if the word is in the trie.
     public boolean search(String word) {
+        if (word == null || word.isEmpty() )
+            return false;
+            
+        TrieNode cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            
+            char c = word.charAt(i);
+            if (cur.children[ c - 'a'] == null)
+                return false;
+                
+            if ( i == word.length() - 1 && cur.children[c-'a'].isLeaf())
+                return true;
+                
+            cur = cur.children[c-'a'];
+        }
         
+        return false;
     }
 
     // Returns if there is any word in the trie
     // that starts with the given prefix.
+    
     public boolean startsWith(String prefix) {
         
+        if (word == null || word.isEmpty() )
+            return false;
+            
+        TrieNode cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            
+            char c = word.charAt(i);
+            if (cur.children[ c - 'a'] == null)
+                return false;
+            cur = cur.children[c-'a'];
+        }
+        
+        return true;        
     }
 }
 
