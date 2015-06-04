53fca2cdfadfbde53cac564bc740263c84b2277b
lllxin037 <lllxin@hotmail.com>
2015-06-03 22:39:32 -0700

    Update Solution.java

--- src/leetcode/shortest/parlindrome/Solution.java
+++ src/leetcode/shortest/parlindrome/Solution.java
@@ -1,18 +1,19 @@
 public class Solution {
     
+    //http://www.felix021.com/blog/read.php?2040
+    
     public String shortestPalindrome(String s) {
         
         if (s == null || s.length() <= 1)
             return s;
             
         char[] arr = s.toCharArray();
-        StringBuilder sb = new StringBuilder(s);
+        StringBuilder sb = new StringBuilder(s).reverse();
         char[] reverseArr = sb.toString().toCharArray();
         
         int maxLen = 0;
-        int maxRowIndex = 0;
-        int maxColIndex = 0;
-        
+        int end = 0;
+
         int n = s.length();
         int[][] dp = new int[n+1][n+1];
         for (int i = 0; i < n; i++) {
@@ -26,14 +27,20 @@ public class Solution {
                 dp[i+1][j+1] = v+1;
                 if ( v+1 > maxLen ) {
                     maxLen = v+1;
-                    maxRowIndex = i;
-                    maxColIndex = j;
+                    end = j;
                 }
             }
         }
         
+        //maxlen must >= 1
         
+        if (maxLen == 1 ) {
+            //no parlindrome at all    
+        } else {
+            int begin = end - maxLen + 1;
+            String parlindrom = sb.substring(begin, end);
+        }
         
-        
+        return sb.toString();
     }
 }
