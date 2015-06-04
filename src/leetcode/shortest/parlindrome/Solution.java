ed90b2ae3aab0d55c1ef57f349fc3d415c745062
lllxin037 <lllxin@hotmail.com>
2015-06-03 21:58:43 -0700

    Create Solution.java

---
+++ src/leetcode/shortest/parlindrome/Solution.java
@@ -0,0 +1,39 @@
+public class Solution {
+    
+    public String shortestPalindrome(String s) {
+        
+        if (s == null || s.length() <= 1)
+            return s;
+            
+        char[] arr = s.toCharArray();
+        StringBuilder sb = new StringBuilder(s);
+        char[] reverseArr = sb.toString().toCharArray();
+        
+        int maxLen = 0;
+        int maxRowIndex = 0;
+        int maxColIndex = 0;
+        
+        int n = s.length();
+        int[][] dp = new int[n+1][n+1];
+        for (int i = 0; i < n; i++) {
+            
+            for (int j = 0; j < n; j++) {
+                
+                if (arr[i] != reverseArr[j]) 
+                    continue;
+                    
+                int v = dp[i][j];
+                dp[i+1][j+1] = v+1;
+                if ( v+1 > maxLen ) {
+                    maxLen = v+1;
+                    maxRowIndex = i;
+                    maxColIndex = j;
+                }
+            }
+        }
+        
+        
+        
+        
+    }
+}
